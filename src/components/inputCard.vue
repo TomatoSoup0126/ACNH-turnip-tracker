0e9968c4bf286d9a7ae9923bd245873c758795ce
TomatoSoup <jonathantang0126@gmail.com>
2020-04-09 14:01:19 +0800

    add input layout

---
+++ src/components/inputCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="col-2">
+   
+        <div class="card-header">
+          {{day.name}}
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            <label>上午</label> 
+            <input type="text" class="form-control" placeholder="$" :id="day.id">
+          </li>
+          <li class="list-group-item">
+             <label>下午</label> 
+            <input type="text" class="form-control" placeholder="$" :id="day.id">
+          </li>
+        </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    day: {
+      type: Array,
+      required: true
+    }
+  }
+
+  
+}
+</script>
--- src/components/priceCard.vue
+++ src/components/priceCard.vue
@@ -1,3 +1,62 @@
 <template>
-  <button type="button" class="btn btn-info">Info</button>
+  <div class="card col-12">
+    <div class="card-body row">
+      <inputCard 
+        v-for="day in weekday" 
+        :key="day.id"
+        :day="day"
+      />
+      
+    </div>
+
+  </div>
 </template>
+
+<script>
+import inputCard from '../components/inputCard.vue'
+
+export default {
+  components:{
+    inputCard
+  },
+
+  data() {
+    return {
+      weekday:[
+        {
+          name:'週一',
+          id:1
+        },
+
+        {
+          name:'週二',
+          id:2
+        },
+
+        {
+          name:'週三',
+          id:3
+        },
+
+        {
+          name:'週四',
+          id:4
+        },
+
+        {
+          name:'週五',
+          id:5
+        },
+
+        {
+          name:'週六',
+          id:6
+        },
+
+      ]
+    }
+    
+  }
+  
+}
+</script>
